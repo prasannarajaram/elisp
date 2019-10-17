e98fb5688440b112be8b50821c9616583e1e0f42
prasannarajaram <prasannarajaram@gmail.com>
2019-10-17 08:24:11 +0530

    My basic elisp file

---
+++ basics.el
@@ -0,0 +1,14 @@
+; 2.5 Exercise
+; ============
+; 
+; Find a file with which you are working and move towards its middle.
+; Find its buffer name, file name, length, and your position in the file.
+
+; This file is used to learn the basics for elisp.
+; The intent of learning elisp is to bring ACM compile in the editor
+
+(buffer-name)"basics.el"
+(buffer-file-name)"/home/amul/Documents/git/elisp/basics.el"
+(point)225 ; Current position in the file
+(/ (point-max) 2)190 ; Goes to the mid point in the file.
+(goto-char (/ (point-max) 2)) ; Goes to the mid point in the file.
